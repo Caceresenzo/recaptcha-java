b9b5041de8182e3ae64af438ff21bbd999b281a0
Caceresenzo <caceresenzo1502@gmail.com>
2025-06-08 01:39:02 +0200

    fix(properties): remove validation

--- v2-spring-boot-starter/src/main/java/dev/caceresenzo/recaptcha/spring/boot/autoconfigure/ReCaptchaV2Properties.java
+++ v2-spring-boot-starter/src/main/java/dev/caceresenzo/recaptcha/spring/boot/autoconfigure/ReCaptchaV2Properties.java
@@ -1,14 +1,11 @@
 package dev.caceresenzo.recaptcha.spring.boot.autoconfigure;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.validation.annotation.Validated;
 
 import dev.caceresenzo.recaptcha.spring.web.annotation.ChallengeResponseLocation;
 import dev.caceresenzo.recaptcha.spring.web.annotation.ReCaptchaV2;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
-@Validated
 @Data
 @ConfigurationProperties(prefix = ReCaptchaV2Properties.PREFIX)
 public class ReCaptchaV2Properties {
@@ -16,13 +13,18 @@ public class ReCaptchaV2Properties {
 	public static final String PREFIX = "recaptcha.v2";
 	public static final String PREFIX_SECRET_KEY = PREFIX + ".secret-key";
 
-	private String secretKey;
-
-	@NotNull
 	private Service service = Service.GOOGLE_RECAPTCHA;
 
-	@NotNull
+	private String secretKey;
+
 	private WebProperties web = new WebProperties();
+
+	public enum Service {
+
+		GOOGLE_RECAPTCHA,
+		CLOUDFLARE_TURNSTILE,
+
+	}
 
 	@Data
 	public class WebProperties {
@@ -30,13 +32,6 @@ public class ReCaptchaV2Properties {
 		private ChallengeResponseLocation location = ChallengeResponseLocation.QUERY;
 		private String headerName = ReCaptchaV2.DEFAULT_HEADER_NAME;
 		private String queryParameterName = ReCaptchaV2.DEFAULT_QUERY_PARAMETER_NAME;
-
-	}
-
-	public enum Service {
-
-		GOOGLE_RECAPTCHA,
-		CLOUDFLARE_TURNSTILE,
 
 	}
 
